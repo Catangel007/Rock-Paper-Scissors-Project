c87c4bacf3a3f269cb90db55f4a3b3ea65d01eb9
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-04-28 22:49:45 +0100

    Add get humanChoicefunction/ included a window prompt to ask the user to select rock,paper,scissor.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 console.log("hello world")
-
 const choices = ["Rock","Paper","Scissors"];
 const random1 = Math.random();
-function getComputerChoice(){
-   random1 = choices[ Math.random(choices.length)];
+
+function getComputerChoice(choices){
+
+   random1 = choices[ (Math.floor(Math.random()*(choices.length)))];
    return random1;
+}
+
+function getHumanChoice(choice){
+    getHumanChoice=window.prompt("Rock, Paper, Scissors shoot!")
+    getHumanChoice= document.querySelector(input)
 }
