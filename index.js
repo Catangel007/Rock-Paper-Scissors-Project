146cb7ee547c31962f590b623329d18d5a03989f
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-05-01 15:01:49 +0100

    Add playRound to code\n this where the games funtionality lies

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-console.log("hello world")
-const choices = ["Rock","Paper","Scissors"];
-const random1 = Math.random();
+
+
+
 let humanScore = 0;
 let computerScore =0;
 humanScore;
 computerScore;
 
-function getComputerChoice(choices){
-
-   random1 = choices[ (Math.floor(Math.random()*(choices.length)))];
-   return random1;
+function getComputerChoice(){
+let myArray = ["Rock","Paper","Scissors"];
+let randomValue = myArray[Math.floor(math.random()*myArray.length)]
+console.log(randomValue);
 }
+getComputerChoice();
 
 function getHumanChoice(choice){
-    getHumanChoice=window.prompt("Rock, Paper, Scissors shoot!, ''")
+    getHumanChoice=window.prompt("Rock, Paper, Scissors, shoot!. ''")
     getHumanChoice= document.querySelector(input)
     console.log(getHumanChoice);
 }
-function playRound(/humanScore/i,computerScore){
-    
+function playRound(humanScore,computerScore){
+
 
 };
