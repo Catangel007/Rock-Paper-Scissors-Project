f53da40e34f5a1e6255caa5f0ac8ff5fcc68b657
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-05-28 22:10:43 +0100

    fix loop: make loop counter display

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-   function getComputerChoice(){
+function getComputerChoice(){
 
     randomNum = Math.floor(Math.random()*3);
     
@@ -12,104 +12,119 @@
         else {
             return "scissors";
     }    
-     }
-     let para = document .querySelector('.score');
+     } getComputerChoice();
+
+
+
+       let para = document .querySelector('.score');
        let gameStats = document.querySelector('.game');
        let message = document.querySelector('.message');
        let winnerStats = document. querySelector('.winner');
        let humanChoice = document.querySelector('.humanChoice');
        let computerChoice = document.querySelector('.computerChoice');
    
-     let humanSelection;
-     let computerSelection;
-     
-        const buttons = document.querySelectorAll("button");
-        buttons.forEach((button) => {
+    
             
-            button.addEventListener("click", (buttons)=> {
-                let humanSelection = button.id;
-                 humanChoice.textContent = `YOU:${humanSelection}  <====> `; 
-                 computerChoice.textContent = `COMPUTER:${computerSelection}`; 
-                  computerSelection = getComputerChoice();  
-            });
-               
-           
-        });
-       
-        console.log(computerSelection); 
-        console.log(humanSelection); 
-         
-      let humanScore  = 0;
-      let computerScore = 0;
 
-       
-
-
-      
-humanSelection;
-computerSelection;
+     let humanScore = 0;
+     let computerScore = 0; 
     
+     
     
-let num;
-
-
-function playGame (){
-
-    for (let i= 1; i <= 5; i++){
-
-    function playRound( computerSelection = getComputerChoice(),  humanSelection= buttons){
-        
-
-        if (computerSelection ==="rock" && humanSelection ==="paper"  || 
-            computerSelection ==="scissors" && humanSelection ==="rock" || 
-            computerSelection ==="paper" && humanSelection ==="scissors")
-        { humanScore++
-            gameStats.textContent ='You Win!'}
-         else if (computerSelection ==="scissors" && humanSelection ==="paper" || 
-                  computerSelection ==="rock" && humanSelection ==="scissors" || 
-                  computerSelection ==="paper" && humanSelection === "rock")//
-        {   computerScore++
-            gameStats.textContent ="Computer Wins!";    
-        } else {
-            gameStats.textContent ="Tie";
-        
-        }  
-}
-    
-    
-    
-  
    
-       
+        
     
-     if (i === 5){
-       
-        para.textContent = "final Round\n";
-        playRound();
-        para.textContent = `GAME-OVER =>\n\n\n Player 🧑‍🚀: ${ humanScore}  |  Computer 💻: ${computerScore}\n\n\n`;
-       // restart();
-       
-     }
-     else{
-        para.textContent = `Round: ${i}`;
-        playRound();
-     }
-    
-  }
-  winner();
   
-} playGame();
+     const buttons = document.querySelectorAll("button");
+     
+        buttons.forEach((button) => { 
+            
+            
+            let humanSelection = "";
+     let computerSelection = getComputerChoice();
+             
+            button.addEventListener("click", ()=> {
+                 humanSelection = button.id;
+                 console.log(humanSelection); 
+                 humanChoice.textContent = `YOU:${humanSelection} `; 
+                 computerChoice.textContent = `COMPUTER:${computerSelection}`; 
+                console.log(computerSelection); 
+                
+                function playRound(human,computer){
 
 
-function winner (){
-    if (humanScore > computerScore){
-        winnerStats.textContent ="Player wins the Game!!";
-    }
-    else if(humanScore < computerScore){
-        winnerStats.textContent ="Computer wins this time";}
-        else { 
-            winnerStats.textContent = "It's a tie";}
-}
+                    if (humanSelection === computerSelection){
+                        gameStats.textContent = "It's a Tie";
+                    } else if (humanSelection === "rock" && computerSelection === "scissors" ||
+                               humanSelection === "paper" && computerSelection === "rock" ||
+                               humanSelection === "scissors" && computerSelection === "paper"){
+                                gameStats.textContent = "You Win this Round!!";
+                                humanScore++;
+                               } else if(humanSelection === "scissors" && computerSelection === "rock" ||
+                                         humanSelection === "rock" && computerSelection === "paper" ||
+                                         humanSelection === "paper" && computerSelection === "scissors"){
+                                gameStats.textContent = "Computer wins this Round.";
+                                computerScore++;
+                               } else{
+                                gameStats.textContent = "Choose your element."
+                                
+                             } 
+                } 
+               
+                playRound(humanSelection, computerSelection);
+                
+ });
+            
+                
+            ;}       
+          );
+       
+//  function playGame (num){
+    
+//     for (let i =1; i <= num ; i++){ 
+//         const result = `${i * i}`;
+//         console.log(`${result}`);
+//         if (i === 5){
+//             console.log(`${"this loop is finished"}`);
+//         } else if(i < 5){
+//             console.log(`${"i am leveling up"}`);
+//         }}
+//  }
+// playGame(5);
+       
+        
+             function playGame(num){
+                
+            
+                      for(let i = 1; i <= num; i++){
+                          const gameRound = `${i == playRound()}`;
+                          playRound()
+                         
+                         if (i === 5){
+                        
+                             para.textContent = `GAME-OVER =>\n\n\n Player 🧑‍🚀: ${ humanScore}  |  Computer 💻: ${computerScore}\n\n\n`;  
+                         } else if(i<5){
+                           
+                             para.textContent = `Round: ${playRound()} => Player 🧑‍🚀: ${ humanScore}  |  Computer 💻: ${computerScore}\n\n\n`;
+                                 
+                         }}//winner();
+                  }
+               playGame(5);
+          
+//      // document.addEventListener("DOMContentLoaded",playGame);      
+     
+//    //}
+    
+//    function winner (){
+//     if (humanScore > computerScore){
+//         winnerStats.textContent ="Player wins the Game!!";
+//     }
+//     else if(humanScore < computerScore){
+//         winnerStats.textContent ="Computer wins this time";}
+//         else { 
+//             winnerStats.textContent = "It's a tie";}
+// }
+    
 
 //function restart(){
 
